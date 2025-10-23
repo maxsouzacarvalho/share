4dc98bc5e2e9d53894305731ec1ae5e772e46331
Max Carvalho <116026624+maxsouzacarvalho@users.noreply.github.com>
2025-10-23 09:58:11 -0300

    Add files via upload

---
+++ lumu_install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget https://raw.githubusercontent.com/maxsouzacarvalho/share/refs/heads/main/lumu_linux_agent_installer_latest.sh
+ /bin/chmod +x lumu_linux_agent_installer_latest.sh
+sudo ./lumu_linux_agent_installer_latest.sh --install --license ebbffb1e-f858-4813-a184-b280947ed401
